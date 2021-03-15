463674b0c6a7da67e910dae6baf3757ccdaa4a53
UVR_JJW_PC\UVR-KAIST <zkrkwlek@gmail.com>
2021-03-15 14:13:40 +0900

    #210315 PC 기능 추가 중. 시스템 파라메터를 관리하는 싱글톤 클래스 추가

--- Assets/Scripts/SystemManager.cs
+++ Assets/Scripts/SystemManager.cs
@@ -1,8 +1,100 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class SystemManager {
+
+    public class InitConnectData
+    {
+        public InitConnectData() { }
+        public InitConnectData(string _userID, string _mapName, bool _bMapping, float _fx, float _fy, float _cx, float _cy, int _w, int _h)
+        {
+            userID = _userID;
+            mapName = _mapName;
+            bMapping = _bMapping;
+            fx = _fx;
+            fy = _fy;
+            cx = _cx;
+            cy = _cy;
+            w = _w;
+            h = _h;
+        }
+        public string userID, mapName;
+        public float fx, fy, cx, cy;
+        public int w, h;
+        public bool bMapping;
+    }
+
+    private static int w, h;
+    private static float fx, fy, cx, cy;
+    public int ImageWidth
+    {
+        get
+        {
+            return w;
+        }
+    }
+    public int ImageHeight
+    {
+        get
+        {
+            return h;
+        }
+    }
+    public float FocalLengthX
+    {
+        get
+        {
+            return fx;
+        }
+    }
+    public float FocalLengthY
+    {
+        get
+        {
+            return fy;
+        }
+    }
+    public float PrincipalPointX
+    {
+        get
+        {
+            return cx;
+        }
+    }
+    public float PrincipalPointY
+    {
+        get
+        {
+            return cy;
+        }
+    }
+    private static string strUserID, strMapName;
+    public string User
+    {
+        get
+        {
+            return strUserID;
+        }
+    }
+    public string Map
+    {
+        get
+        {
+            return strMapName;
+        }
+    }
+    private static string serveraddr;
+    public string ServerAddr
+    {
+        get
+        {
+            return serveraddr;
+        }
+    }
+
     static private SystemManager m_pInstance = null;
     static public SystemManager Instance
     {
@@ -10,12 +102,69 @@ public class SystemManager {
             if (m_pInstance == null)
             {
                 m_pInstance = new SystemManager();
+                //LoadParameter();
             }
             return m_pInstance;
         }
     }
-    static public void LoadParameter()
-    {
 
+    static bool bconnect = false;
+    static string[] imgFileLIst;
+    static string imgPathTxt;
+    static int nImgFrameIDX = 3;
+    static bool bCam = false;
+    static bool bMapping;
+
+    public void LoadParameter()
+    {
+        Debug.Log("Load Parameter!!");
+        string strParameterFilePath = Application.persistentDataPath + "/param.txt";
+        string[] paramText = File.ReadAllLines(strParameterFilePath);
+        int nUserData = 0;
+        strUserID = (paramText[nUserData++].Split('=')[1]);
+        serveraddr = (paramText[nUserData++].Split('=')[1]);
+        bool bMapLoad = Convert.ToBoolean(paramText[nUserData++].Split('=')[1]);
+        bool bMapReset = Convert.ToBoolean(paramText[nUserData++].Split('=')[1]);
+        bMapping = Convert.ToBoolean(paramText[nUserData++].Split('=')[1]);
+        strMapName = (paramText[nUserData++].Split('=')[1]);
+        string datafile = (paramText[nUserData++].Split('=')[1]);
+
+        string[] dataText = File.ReadAllLines(Application.persistentDataPath + datafile); //데이터 읽기
+
+        if (datafile == "/File/cam.txt")
+        {
+            bCam = true;
+        }
+
+        int numLine = 0;
+        if (!bCam)
+        {
+            string imgFileTxt = Application.persistentDataPath + Convert.ToString(dataText[numLine++].Split('=')[1]);
+            imgFileLIst = File.ReadAllLines(imgFileTxt);
+            Debug.Log("Load Datase = " + (imgFileLIst.Length - 3));
+            imgPathTxt = Convert.ToString(dataText[numLine++].Split('=')[1]);
+            if (Application.platform == RuntimePlatform.Android)
+                imgPathTxt = Application.persistentDataPath + imgPathTxt;
+        }
+        else
+        {
+            numLine = 2;
+        }
+
+        fx = Convert.ToSingle(dataText[numLine++].Split('=')[1]);
+        fy = Convert.ToSingle(dataText[numLine++].Split('=')[1]);
+        cx = Convert.ToSingle(dataText[numLine++].Split('=')[1]);
+        cy = Convert.ToSingle(dataText[numLine++].Split('=')[1]);
+        w = Convert.ToInt32(dataText[numLine++].Split('=')[1]);
+        h = Convert.ToInt32(dataText[numLine++].Split('=')[1]);
+        
+        ////이건 뎁스소스에서 가져가도록 해야 함
+        //DepthSource.Width = w;
+        //DepthSource.Height = h;
+    }
+
+    public InitConnectData GetConnectData()
+    {
+        return new InitConnectData(strUserID, strMapName, bMapping, fx, fy, cx, cy, w, h);
     }
 }
--- Assets/Scripts/UVRSystem.cs
+++ Assets/Scripts/UVRSystem.cs
@@ -5,234 +5,45 @@ using System.IO;
 using System.Runtime.InteropServices;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class UVRSystem : MonoBehaviour
 {
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-
-    //}
-    static string[] imgFileLIst;
-    static string imgPathTxt;
-    static int nImgFrameIDX = 3;
-    static bool bCam = false;
-    static bool bMapping;
-
-    private static string serveraddr;
-    public static string ServerAddr
-    {
-        get
-        {
-            return serveraddr;
-        }
-    }
-    
-    private static bool bconnect = false;
-    //public static bool Connect
-    //{
-    //    get
-    //    {
-    //        return bconnect;
-    //    }
-    //}
-
-    private static string strUserID, strMapName;
-    public static string User
-    {
-        get
-        {
-            return strUserID;
-        }
-    }
-    public static string Map
-    {
-        get
-        {
-            return strMapName;
-        }
-    }
-    private static int w, h;
-    private static float fx, fy, cx, cy;
-    public static int ImageWidth
-    {
-        get
-        {
-            return w;
-        }
-    }
-    public static int ImageHeight
-    {
-        get
-        {
-            return h;
-        }
-    }
-    public static float FocalLengthX
-    {
-        get
-        {
-            return fx;
-        }
-    }
-    public static float FocalLengthY
-    {
-        get
-        {
-            return fy;
-        }
-    }
-    public static float PrincipalPointX
-    {
-        get
-        {
-            return cx;
-        }
-    }
-    public static float PrincipalPointY
-    {
-        get
-        {
-            return cy;
-        }
-    }
-
-    public class InitConnectData
-    {
-        public InitConnectData() { }
-        public InitConnectData(string _userID, string _mapName, bool _bMapping, float _fx, float _fy, float _cx, float _cy, int _w, int _h)
-        {
-            userID = _userID;
-            mapName = _mapName;
-            bMapping = _bMapping;
-            fx = _fx;
-            fy = _fy;
-            cx = _cx;
-            cy = _cy;
-            w = _w;
-            h = _h;
-        }
-        public string userID, mapName;
-        public float fx, fy, cx, cy;
-        public int w, h;
-        public bool bMapping;
-    }
-
+        
     static GCHandle webCamHandle;
     static WebCamTexture webCamTexture;
     [HideInInspector]
     static public Color[] webCamColorData;
     static IntPtr webCamPtr;
-
-    public RawImage background;
-
-    //[MenuItem("UVRSystem/Load Parameter")]
-    static void LoadParam()
-    {
-        string strParameterFilePath = Application.persistentDataPath + "/param.txt";
-        string[] paramText = File.ReadAllLines(strParameterFilePath);
-        int nUserData = 0;
-        strUserID = (paramText[nUserData++].Split('=')[1]);
-        serveraddr = (paramText[nUserData++].Split('=')[1]);
-        bool bMapLoad = Convert.ToBoolean(paramText[nUserData++].Split('=')[1]);
-        bool bMapReset = Convert.ToBoolean(paramText[nUserData++].Split('=')[1]);
-        bMapping = Convert.ToBoolean(paramText[nUserData++].Split('=')[1]);
-        strMapName = (paramText[nUserData++].Split('=')[1]);
-        string datafile = (paramText[nUserData++].Split('=')[1]);
-        Debug.Log(strUserID + ":" + bMapping + "::" + datafile);
-
-        string[] dataText = File.ReadAllLines(Application.persistentDataPath + datafile); //데이터 읽기
-
-        Debug.Log(Application.persistentDataPath + datafile + "::" + dataText.Length);
-
-        //strMapName = datafile.Split('/')[2].Split('.')[0];
-        //Debug.Log(strMapName);
-
-        if (datafile == "/File/cam.txt")
-        {
-            bCam = true;
-        }
-
-        int numLine = 0;
-        if (!bCam)
-        {
-            string imgFileTxt = Application.persistentDataPath + Convert.ToString(dataText[numLine++].Split('=')[1]);
-            imgFileLIst = File.ReadAllLines(imgFileTxt);
-            Debug.Log("Load Datase = " + (imgFileLIst.Length - 3));
-            imgPathTxt = Convert.ToString(dataText[numLine++].Split('=')[1]);
-            if (Application.platform == RuntimePlatform.Android)
-                imgPathTxt = Application.persistentDataPath + imgPathTxt;
-        }
-        else
-        {
-            numLine = 2;
-        }
-
-        fx = Convert.ToSingle(dataText[numLine++].Split('=')[1]);
-        fy = Convert.ToSingle(dataText[numLine++].Split('=')[1]);
-        cx = Convert.ToSingle(dataText[numLine++].Split('=')[1]);
-        cy = Convert.ToSingle(dataText[numLine++].Split('=')[1]);
-        w = Convert.ToInt32(dataText[numLine++].Split('=')[1]);
-        h = Convert.ToInt32(dataText[numLine++].Split('=')[1]);
-        Debug.Log(fx + " " + fy);
-        DepthSource.Width = w;
-        DepthSource.Height = h;
-
-        if (bCam)
-        {
-            //#if UNITY_ANDROID
-            //            w = 640;
-            //            h = 360;
-            //#endif
-            //#if UNITY_EDITOR_WIN
-            //            w = 320*2;
-            //            h = 240*2;
-            //#endif
-            WebCamDevice[] devices = WebCamTexture.devices;
-            for (int i = 0; i < devices.Length; i++)
-            {
-                if (Application.platform == RuntimePlatform.Android && !devices[i].isFrontFacing)
-                {
-                    webCamTexture = new WebCamTexture(devices[i].name, w, h, 30);
-                    break;
-                }
-                else if (Application.platform == RuntimePlatform.WindowsEditor && devices[i].isFrontFacing)
-                {
-
-                    webCamTexture = new WebCamTexture(devices[i].name, w, h, 30);
-                    Debug.Log(devices[i].name + "::" + webCamTexture.requestedWidth + ", " + webCamTexture.requestedHeight);
-                    break;
-                }
-            }
-            //webCamTexture.Play();
-            //////webcam image pointer 연결
-            //webCamColorData = new Color[w * h];
-            ////webCamHandle = default(GCHandle);
-            //webCamHandle = GCHandle.Alloc(webCamColorData, GCHandleType.Pinned);
-            //webCamPtr = webCamHandle.AddrOfPinnedObject();
-            //background.texture = webCamTexture;
-        }
-
-    }
-
+    
     [MenuItem("UVRSystem/Connect")]
     static void Connect()
     {
         //id, map, mapping 여부, fx, fy, cx, cy, w, h
-        LoadParam();
+        SystemManager.Instance.LoadParameter();
+        SystemManager.InitConnectData data = SystemManager.Instance.GetConnectData();
+        string msg = JsonUtility.ToJson(data);
+        byte[] bdata = System.Text.Encoding.UTF8.GetBytes(msg);
+
+        UnityWebRequest request = new UnityWebRequest(SystemManager.Instance.ServerAddr+"/Connect");
+        request.method = "POST";
+        UploadHandlerRaw uH = new UploadHandlerRaw(bdata);
+        uH.contentType = "application/json";
+        request.uploadHandler = uH;
+        request.downloadHandler = new DownloadHandlerBuffer();
+        UnityWebRequestAsyncOperation res = request.SendWebRequest();
     }
 
     [MenuItem("UVRSystem/Disconnect")]
-    void Disconnect()
+    static void Disconnect()
     {
-        Debug.Log("Doing Something...");
+        string addr = SystemManager.Instance.ServerAddr + "/Disconnect?userID=" + SystemManager.Instance.User;
+        UnityWebRequest request = new UnityWebRequest(addr);
+        request.method = "POST";
+        request.downloadHandler = new DownloadHandlerBuffer();
+        UnityWebRequestAsyncOperation res = request.SendWebRequest();
+        Debug.Log("Disconnect!!"+ addr);
     }
 
     [MenuItem("UVRSystem/Reset")]
@@ -250,6 +61,10 @@ public class UVRSystem : MonoBehaviour
     [MenuItem("UVRSystem/Load Map")]
     static void LoadMap()
     {
-        Debug.Log("Doing Something...");
+        string addr = SystemManager.Instance.ServerAddr + "/LoadMap?map=" + SystemManager.Instance.Map;
+        UnityWebRequest request = new UnityWebRequest(addr);
+        request.method = "POST";
+        request.downloadHandler = new DownloadHandlerBuffer();
+        UnityWebRequestAsyncOperation res = request.SendWebRequest();
     }
 }
